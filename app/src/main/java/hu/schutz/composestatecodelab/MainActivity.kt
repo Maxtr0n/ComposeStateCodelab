b98517e03af8a5c0448e8970ffcc99019190c2be
Schütz Máté <schutz.mate@gmail.com>
2022-11-02 17:05:00 +0100

    Complete codelab

--- app/src/main/java/hu/schutz/composestatecodelab/MainActivity.kt
+++ app/src/main/java/hu/schutz/composestatecodelab/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import hu.schutz.composestatecodelab.ui.theme.ComposeStateCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +24,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WellnessScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeStateCodelabTheme {
-        Greeting("Android")
     }
 }
